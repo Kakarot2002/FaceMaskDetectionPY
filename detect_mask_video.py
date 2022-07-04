a6ac4ebf714843852b75f96afa508a51c41b0588
Kakarot2002 <11931@cic.du.ac.in>
2022-07-04 09:36:13 +0530

    made my me message

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -9,6 +9,7 @@ import time
 import cv2
 import os
 
+# made by prabhat 
 def detect_and_predict_mask(frame, faceNet, maskNet):
 	# grab the dimensions of the frame and then construct a blob
 	# from it
