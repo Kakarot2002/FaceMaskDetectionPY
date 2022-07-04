e013f26afef26929e31647988f9e3c717f7bf0c4
Prabhat Gupta <106004670+Kakarot2002@users.noreply.github.com>
2022-07-04 10:39:06 +0530

    Add comments

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -116,7 +116,7 @@ while True:
 			cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
 		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
 
-	# show the output frame
+	# show the output frames 
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
 
